62d1cb0cc5daebedecf925eb2d789752edbe4098
Sandusha20049887 <sandusha1997@gmail.com>
2024-11-17 22:45:50 +0000

    triggers

---
+++ Triggers.sql
@@ -0,0 +1,34 @@
+--Trigger to update cashbook when vehicle purchase from car dealership
+CREATE TRIGGER UpdateCashBookOnVehicleAdd
+ON vehicles
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO cash_book (vehicle_id, debited_amount, transaction_date)
+    SELECT 
+        vehicle_id, price, record_date     
+    FROM 
+        inserted;
+END;
+
+----Trigger to update cashbook when vehicle sales from car dealership and update vehicle status as sold
+CREATE TRIGGER UpdateCashBookOnVehicleSale
+ON sales
+AFTER INSERT
+AS
+BEGIN
+    -- Update vehicles table to set status to 'Sold' 
+    UPDATE vehicles 
+    SET status_id = 'SL' 
+    FROM vehicles 
+    WHERE vehicle_id = vehicle_id;
+
+    -- Insert into cash_book table
+    INSERT INTO cash_book (vehicle_id, credited_amount, transaction_date)
+    SELECT 
+        vehicle_id, price, sale_date
+    FROM 
+        inserted;
+END;
+
+
