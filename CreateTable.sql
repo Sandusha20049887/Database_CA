ac7af2f5d552982f241e62a2737d66852fb7dc69
Sandusha20049887 <sandusha1997@gmail.com>
2024-11-18 22:20:59 +0000

    changes

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,32 +1,34 @@
-create database car_dealership_grp7;
-use car_dealership_grp7;
+
+create database car_dealership_new;
+
+use car_dealership_new;
 --creating tables
 
 CREATE TABLE roles (
     role_id VARCHAR(10) PRIMARY KEY,
-    role_name VARCHAR(50) NOT NULL
+    role VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE branches (
     branch_id VARCHAR(5) PRIMARY KEY,
     branch_name VARCHAR(50) NOT NULL,
-    branch_address VARCHAR(500),
+    address VARCHAR(500),
     phone VARCHAR(15)
 );
 
 CREATE TABLE make (
-    make_id INT PRIMARY KEY,
+    make_id INT IDENTITY(1,1) PRIMARY KEY,
     make VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE status (
     status_id VARCHAR(2) PRIMARY KEY,
-    status_description VARCHAR(20) NOT NULL
+    status VARCHAR(20) 
 );
 
 CREATE TABLE customers (
-    customer_id INT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
+    customer_id INT IDENTITY(1,1) PRIMARY KEY,
+    first_name VARCHAR(50),
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     phone VARCHAR(15),
@@ -35,24 +37,24 @@ CREATE TABLE customers (
 );
 
 CREATE TABLE employees (
-    employee_id INT PRIMARY KEY,
-    role_id VARCHAR(10) NOT NULL,
-    branch_id VARCHAR(5) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
+    employee_id INT IDENTITY(1,1) PRIMARY KEY,
+    role_id VARCHAR(10),
+    branch_id VARCHAR(5),
+    first_name VARCHAR(50),
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
     phone VARCHAR(15),
     joined_date DATE,
-	FOREIGN KEY (role_id) REFERENCES roles(role_id),
-	FOREIGN KEY (branch_id) REFERENCES branches(branch_id)
+	CONSTRAINT Employees_Roles_Fk FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE,
+	CONSTRAINT Employees_Branches_Fk FOREIGN KEY (branch_id) REFERENCES branches(branch_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE model (
-    model_id INT PRIMARY KEY,
-    make_id INT NOT NULL,
-    model VARCHAR(50) NOT NULL,
-	FOREIGN KEY (make_id) REFERENCES make(make_id),
+    model_id INT IDENTITY(1,1) PRIMARY KEY,
+    make_id INT,
+    model VARCHAR(50),
+	CONSTRAINT Model_Make_FK FOREIGN KEY (make_id) REFERENCES make(make_id)
 );
 
 CREATE TABLE vehicles (
@@ -61,50 +63,51 @@ CREATE TABLE vehicles (
     employee_id INT,
     model_id INT,
     vin VARCHAR(10) UNIQUE NOT NULL,
-    manfac_year INT,
+    year INT,
     price DECIMAL(10,2),
     specifications XML,
     record_date DATE,
-	FOREIGN KEY (status_id) REFERENCES status(status_id),
-	FOREIGN KEY (employee_id) REFERENCES employees(employee_id),
-	FOREIGN KEY (model_id) REFERENCES model(model_id)
+	CONSTRAINT Vehicles_Status_Fk FOREIGN KEY (status_id) REFERENCES status(status_id) ON DELETE CASCADE,
+	CONSTRAINT Vehicles_Employees_Fk FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE,
+	CONSTRAINT Vehicles_Model_Fk FOREIGN KEY (model_id) REFERENCES model(model_id) ON DELETE CASCADE
 );
 
 CREATE TABLE sales (
-    sales_id INT PRIMARY KEY,
+    sales_id INT IDENTITY(1,1) PRIMARY KEY,
     customer_id INT,
     vehicle_id VARCHAR(7),
     employee_id INT,
     sale_date DATE,
     price DECIMAL(10,2),
-	FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-	FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id),
-	FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+	CONSTRAINT FK_Sales_Customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE,
+	CONSTRAINT FK_Sales_Vehicles FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id) ON DELETE NO ACTION,
+	CONSTRAINT FK_Sales_Employees FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE
 );
 
 CREATE TABLE service_records (
-    service_id INT PRIMARY KEY,
+    service_id INT IDENTITY(1,1) PRIMARY KEY,
     vehicle_id VARCHAR(7),
     branch_id VARCHAR(5),
     service_date DATE,
     cost DECIMAL(10,2),
     service_details XML,
-	FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id),
-	FOREIGN KEY (branch_id) REFERENCES branches(branch_id)
+	CONSTRAINT ServiceRecords_Vehicles_Fk FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id) ON DELETE CASCADE,
+	CONSTRAINT ServiceRecords_Branches_Fk FOREIGN KEY (branch_id) REFERENCES branches(branch_id) ON DELETE NO ACTION
 );
 
 CREATE TABLE media (
-    media_id INT PRIMARY KEY,
+    media_id INT IDENTITY(1,1) PRIMARY KEY,
     vehicle_id VARCHAR(7),
     media_path VARCHAR(500),
-	FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id)
+	CONSTRAINT Media_Vehicles_Fk FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id) ON DELETE CASCADE
 );
 
 CREATE TABLE cash_book (
-    cash_book_id INT PRIMARY KEY,
+    cash_book_id INT IDENTITY(1,1) PRIMARY KEY,
     vehicle_id VARCHAR(7),
-    debited_amount DECIMAL(10,2),
-    credited_amount DECIMAL(10,2),
-    transaction_date DATE,
-	FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id)
+    debited_amount DECIMAL(10,2) DEFAULT 0.00,
+    credited_amount DECIMAL(10,2) DEFAULT 0.00,
+    date DATE,
+	CONSTRAINT CashBook_Vehicles_Fk FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id) ON DELETE CASCADE
 );
+
